598d106677399a7210fd20a169768e0faef81d87
paul-thiers <162619089+paul-thiers@users.noreply.github.com>
2024-03-15 11:48:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Avancement TIPE - Paul CHAMPSAUR MP2I</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+  <header>
+  <?php echo '<p>Bonjour le monde</p>'; ?>
+    <h1>Avancement TIPE - Paul CHAMPSAUR MP2I</h1>
+  </header>
+  <main>
+    <table>
+      <tr>
+        <th>Dates</th>
+        <th>Recherches</th>
+      </tr>
+      <!-- Vous pouvez remplir ce tableau avec vos données -->
+      <tr>
+        <td>16 février</td>
+        <td>- Modélisation mathématique et simulation de phénomènes de diffusion dans les réseaux/ différents types d'algorithmes<br>
+          - modélisation réseaux neuronaux<br>
+          - prédiction des phénomênes sismiques<br>
+          - transition => véhicules électriques, hygrogène ??<br>
+        </td>
+      </tr>
+      <tr>
+        <td>23 février</td>
+        <td>- Convertion, transition => grandeurs, énergie (HYDROGÈNE),<br> 
+          - Voiture hydrogènes, son fonctionnement, rentabilité énergétique : le pourcentage de l’énergie consommée (ou énergie absorbée) et transformée en énergie utile (stockage hydrogène, pile à combustible)<br>
+          https://www.connaissancedesenergies.org/tribune-actualite-energies/comment-fonctionnent-les-voitures-hydrogene<br>
+          https://www.renaultgroup.com/news-onair/actualites/comment-fonctionne-un-vehicule-a-hydrogene/<br>
+          https://fr.wikipedia.org/wiki/V%C3%A9hicule_%C3%A0_hydrog%C3%A8ne<br>
+          https://www.h2-mobile.fr/dossiers/fonctionnement-voiture-hydrogene-comment-ca-marche/<br>
+          https://www.autonews.fr/green/voiture-hydrogene</td>
+      </tr>
+      <tr>
+        <td>02 mars</td>
+        <td>- Différentes méthodes d'apprentissage automatique pour la reconnaissance d'images (médicales, reconnaissance faciale...)<br>
+          https://www.kaspersky.fr/resource-center/definitions/what-is-facial-recognition<br>
+          https://datascientest.com/reconnaissance-faciale-tout-savoir<br>
+          https://intelligence-artificielle.com/reconnaissance-faciale-guide-complet/<br>
+          https://france.devoteam.com/paroles-dexperts/mieux-comprendre-le-deep-learning-applique-a-la-reconnaissance-dimages/<br>
+          https://skimai.com/fr/les-differents-types-dapprentissage-automatique/
+        </td>
+      </tr>
+      <!-- Ajoutez autant de lignes que nécessaire -->
+    </table>
+  </main>
+</body>
+</html>
